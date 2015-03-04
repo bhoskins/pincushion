bcd121b35a3793ccbf8e2771a81d25dfdaddbabe
Jacob Smith <jacob@jacobsmith.io>
2015-03-04 16:14:58 -0500

    Simplify route

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,14 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(){
-    return Ember.RSVP.hash({
-      bookmarks: this.store.findAll('bookmark'),
-      users: Ember.RSVP.resolve([])
-    });
+    return this.store.findAll('bookmark'),
   },
-
-  setupController: function(controller, model){
-    controller.set('model', model.bookmarks);
-    controller.set('users', model.users);
-  }
 });
