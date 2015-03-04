b550c0c54807c20a5dffc198a459ef50f7cba3c9
Jacob Smith <jacob@jacobsmith.io>
2015-03-04 16:43:55 -0500

    Finish adapters/bookmark

--- app/adapters/bookmark.js
+++ app/adapters/bookmark.js
@@ -1,7 +1,7 @@
 import ajax from 'ic-ajax';
 import Ember from 'ember';
 
-// TODO: reverse id -> objectId for POST/PUT
+// TODO: reverse id -> objectId for POST/PUT/DELETE
 
 export default Ember.Object.extend({
   find: function(name, id){
@@ -24,8 +24,35 @@ export default Ember.Object.extend({
     });
   },
 
-  createRecord: null,
-  deleteRecord: null,
-  saveRecord: null
+  destroy: function(name, record) {
+    /* jshint unused: false */
+    return ajax({
+      url: "https://api.parse.com/1/classes/Bookmark/" + record.id,
+      type: "DELETE"
+    });
+  },
 
+  save: function(name, record) {
+    /* jshint unused: false */
+    if(record.id) {
+      return ajax({
+        url: "https://api.parse.com/1/classes/Bookmark/" + record.id,
+        type: "PUT",
+        data: JSON.stringify(record)
+      }).then(function(response) {
+        response.id = response.objectId;
+        delete response.objectId;
+        return response;
+      });
+    } else {
+      return ajax({
+        url: "https://api.parse.com/1/classes/Bookmark",
+        type: "POST",
+        data: JSON.stringify(record)
+      }).then(function(response) {
+        record.updatedAt = response.updatedAt;
+        return record;
+      });
+    }
+  }
 });
