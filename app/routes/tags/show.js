16fd74877c769dc7285c090bee249543f53975aa
Jacob Smith <jacob@jacobsmith.io>
2015-03-05 09:52:46 -0500

    Fix bug

--- app/routes/tags/show.js
+++ app/routes/tags/show.js
@@ -4,7 +4,12 @@ export default Ember.Route.extend({
   model: function(params){
     return this.store.findAll('bookmark').then(function(bookmarks) {
       return bookmarks.filter(function(bookmark) {
-        return bookmark.get('tags').contains(params.tag);
+        var tags = bookmark.get('tags')
+        if(tags) {
+          return tags.contains(params.tag);
+        } else {
+          return false;
+        }
       });
     });
   },
