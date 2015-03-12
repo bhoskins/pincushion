affe402153997bc2365b854eb18d6e9d90d4e5d4
Jacob Smith <jacob@jacobsmith.io>
2015-03-12 08:49:32 -0400

    Proxy save and destroy to store

--- app/models/bookmark.js
+++ app/models/bookmark.js
@@ -1,6 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Object.extend({
+  destroy: function(){
+    this.store.destroy('bookmark', this);
+  },
+
+  save: function(){
+    this.store.save('bookmark', this);
+  },
+
   toJSON: function(){
     return this;
   }
